74758ec728e7fac4c1a4dfdcc7d3b4549309e131
manpreet8021 <54615943+manpreet8021@users.noreply.github.com>
2024-01-05 00:47:40 +0530

    added slice

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,17 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import findPlaceSlice from "../features/hero/findPlaceSlice";
 import searchSliceReducer from './slice/searchSlice';
-import commonSlice from './slice/commonSlice'
+import commonSliceReducer from './slice/commonSlice'
+import authSliceReducer from "./slice/authSlice";
+import { apiSlice } from "./slice/apiSlice";
 
 export const store = configureStore({
     reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer, 
         hero: findPlaceSlice,
         search: searchSliceReducer,
-        common: commonSlice
+        common: commonSliceReducer,
+        auth: authSliceReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
     devTools: true
